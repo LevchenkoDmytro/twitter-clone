0745cc0e6696c4190bc2c8a6f24209659c363c4a
Dima Levchenko <112283201+levandowskiy@users.noreply.github.com>
2024-10-20 16:48:12 +0300

    for separate deployment of frontend and backend

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,3 @@
-import path from 'path'
 import express from "express";
 import authRoutes from './routes/auth.route.js'
 import userRoutes from './routes/user.route.js'
@@ -14,13 +13,19 @@ dotenv.config()
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const __dirname = path.resolve()
+
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 })
+
+app.use(cors({
+  origin: 'http://localhost:3000',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+  credentials: true 
+}));
 
 app.use(express.json({limit: '5mb'}))
 app.use(express.urlencoded({extended:true}))
@@ -30,14 +35,6 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postsRoutes);
 app.use("/api/notifications", notificationRoutes);
-
-if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "/frontend/dist")));
-
-	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-	});
-}
 
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
